8de2d8d71226142c41321a8cfc1bb8ba984255d1
Mikael <akopyanmikael88@gmail.com>
2025-07-09 15:02:27 +0400

    Rename Bitwize => C_Bitwize_JUNE_30

---
+++ C_Bitwize_JUNE_30/uppercase.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+    int main(){
+	 int x = 0;
+	    printf("Pls input the number");
+	    scanf("%d" , &x);
+	 int res = 1 << x ; 
+    	    printf("result : %d" , res);
+	
+ 	 return 0;
+	
+	
+	
+	
+	}

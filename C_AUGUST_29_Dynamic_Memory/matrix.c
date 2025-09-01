31f9ee4484e3323c1725a377e4db639367d38f2c
Mikael <akopyanmikael88@gmail.com>
2025-09-01 22:05:14 +0400

    corrected taks

--- C_AUGUST_29_Dynamic_Memory/matrix.c
+++ C_AUGUST_29_Dynamic_Memory/matrix.c
@@ -5,30 +5,42 @@ int main() {
     int r = 0; 
     int c = 0;
     
-    printf("Please input number for row");
+    printf("Please input number for row\n");
     scanf("%d", &r);
     
-    printf("Please input number for column");
+    printf("Please input number for column\n");
     scanf("%d", &c);
     
-    int* arr = malloc(r * sizeof(int));
-    int* arr1[r] =  malloc(c *sizeof(int));
+    int** matrix = (int**) malloc(r * sizeof(int*));
+      
     
-    for (int i = 0;i < c;++i) {
-        for (int j = 0;j < r;++j) {
+    for (int i = 0; i < r; ++i) {
+        matrix[i] = (int*) malloc(sizeof(int) * c);
+        if (matrix[i] == NULL) {
+            for (int j = i-1; j >= 0; --j) free(matrix[j]);
+        }
+    } 
+
+    
+    for (int i = 0;i < r;++i) {
+        for (int j = 0;j < c;++j) {
             printf("PLease input number for [%d][%d]:\n", i, j);
-            scanf("%d", &arr1[j][i]);
+            scanf("%d", &matrix[i][j]);
         }
     }
     
-    printf("This is your size: %lu\n", sizeof(arr));
+    for (int i = 0; i < r; ++i) {
+        for (int j = 0; j < c; ++j) {
+            if (i >= j) matrix[i][j] = 0;
+        }
+    }
+    printf("This is your size: %lu\n", sizeof(matrix));
     
     printf("Your number is:\n");
 
-    for (int i = 0;i < c;++i) {
-         for (int j = 0;j < r;++j) {
-             printf("[%d][%d]: %d\n", i, j, arr[i][j]);
-         }
+    for (int i = 0;i < r;++i) {
+         for (int j = 0;j < c;++j) printf("%d", matrix[i][j]);
+        printf("\n");
      }
 return 0;
 
